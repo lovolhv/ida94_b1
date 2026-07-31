64543536f72b0cb80c55923dfaa4e1afee4e7f9a
yigods <2781431072@qq.com>
2026-08-01 07:25:53 +0800

    Deobfuscate IDA 9.4 license generator

--- kg_patch/全平台注册机IDA94b1.py
+++ kg_patch/全平台注册机IDA94b1.py
@@ -1,114 +1,155 @@
-import base64 as r2XktcCPT8ErM3, zlib as TMgsPi09UJHQHu
-LYBZtNRmqOYql0 = 18260
-while LYBZtNRmqOYql0 != 0:
-    if LYBZtNRmqOYql0 == 26496:
-        Qjg3jQgVCqMeE6[21] = "9!dC%oGUHn;5qN@8q6b7Ru?6&F9!%`bKAMO4GsS#6{1zLFo<S6KZjdd_wdTN3Pl5=!5`wa(60q8%y18YBWRgM%i6$3m@61|3N!=y*"
-        LYBZtNRmqOYql0 = 38730
-    elif LYBZtNRmqOYql0 == 13323:
-        Qjg3jQgVCqMeE6[24] = "0IPA^@pjJ_2I{;Zb32MIHLCkjU1IQ8M=cK|hMQZV)UI6r{X8~5J$PSyxrpd)>E0W-8$QQGD)p&}HgYGwGRh*`~w^>$+0f052KdW)i"
-        LYBZtNRmqOYql0 = 25136
-    elif LYBZtNRmqOYql0 == 72572:
-        U0Jkc1XYutt5qJ = getattr(r2XktcCPT8ErM3, 'b85decode')(WlYdRPK4Lb2E6U)
-        LYBZtNRmqOYql0 = 37644
-    elif LYBZtNRmqOYql0 == 93390:
-        Qjg3jQgVCqMeE6[4] = "~a~9Wq^7-+D`=k<O%ChvO(xpTk-`O)P*CWN5D9I~r&MvZoacx9~!{O^j$)c)@8QH&gyk}JP1$>?}F4I8tDXXRgv5f^MOa?TmL`_1M"
-        LYBZtNRmqOYql0 = 85257
-    elif LYBZtNRmqOYql0 == 56436:
-        Qjg3jQgVCqMeE6[7] = "k}GoHj-(PMKO(FG@&T)bmBHBv)&kYnlft1#qV>5Wb)xS6iWna+0+zGn1uJ_w!kWbjv+t7-WAI3B@eKE5ka2o3boa5y?6bOYdbqB-a"
-        LYBZtNRmqOYql0 = 35338
-    elif LYBZtNRmqOYql0 == 75579:
-        Qjg3jQgVCqMeE6[16] = "@N_PKi|Lk_WhgRXoGdBV#d-;ioJu<%u?}-vvfZE`s*Q1<4w}mp$gO|Yb)}5b!A5Ja8bo&I<Ff?I}sa_m&S0FiE%33*KnCJaGTji+z"
-        LYBZtNRmqOYql0 = 10868
-    elif LYBZtNRmqOYql0 == 37644:
-        jG0wAIJ6QIBxHV = getattr(TMgsPi09UJHQHu, 'decompress')(U0Jkc1XYutt5qJ).decode('utf-8')
-        LYBZtNRmqOYql0 = 83264
-    elif LYBZtNRmqOYql0 == 18260:
-        Qjg3jQgVCqMeE6 = {}
-        LYBZtNRmqOYql0 = 44694
-    elif LYBZtNRmqOYql0 == 49439:
-        Qjg3jQgVCqMeE6[28] = "PauNzBwIuTV3;uiT2t0}WwPw&JGVHEnSuBoRp)aJ0^&{c*2f0{Q0CF@ZOepp$u3mu57QSs;m2x}x!)@L#>dLy8J3hGw7+<_8RH5hC"
-        LYBZtNRmqOYql0 = 26305
-    elif LYBZtNRmqOYql0 == 26305:
-        Qjg3jQgVCqMeE6[29] = "4MW{)xLTewCtxg}gr%RSkgqmOW62*6tN=1+4e_xRXqhZ{2qlSc@%37nFoS6O!;I475c{%E~WRo35*0v^8&zgy*6<jWy(2;Z;B9zxs"
-        LYBZtNRmqOYql0 = 36525
-    elif LYBZtNRmqOYql0 == 38730:
-        Qjg3jQgVCqMeE6[22] = "%FThzt;fB#S(5P=)c|eX6D}!JA%0&)VqX3V+lAxevj(7RA-2f?qQUErAn0KPFElq27PmA4(J;MWqQYIcbi-<gt({28do&ucWcvY1)"
-        LYBZtNRmqOYql0 = 15643
-    elif LYBZtNRmqOYql0 == 32451:
-        Qjg3jQgVCqMeE6[11] = "RxjYh@6Bu0rx}K41Cv%P0+fIXL|vBN_@duV=;rMP}lRKB#2xqGQCK|)V17z2ae?lgaXV+0E;Yc`vJ9L3RNLboY<nB&ma(i$1RsZP8"
-        LYBZtNRmqOYql0 = 61188
-    elif LYBZtNRmqOYql0 == 41789:
-        Qjg3jQgVCqMeE6[31] = "2^0Gw<{spHIrqmli_)p;<_NUG!oIq@><tAP$-vck;|y8D_EmG?i?CvgAckXgN2Nb#}aLf$1>yEcq~IV9&3#`O&ojw25TZ;1p"
-        LYBZtNRmqOYql0 = 15251
-    elif LYBZtNRmqOYql0 == 99127:
-        Qjg3jQgVCqMeE6[1] = "?a$sMT~KE)w!Qjr|PR~KigSU<xZ65J0d?L^NV^~<aX~GF+Md&T<|oXjL4!+j6lxxG)?Or+}b0DWkJHBL$Wj$c_qkmaWN4&(c`kHDr"
-        LYBZtNRmqOYql0 = 97640
-    elif LYBZtNRmqOYql0 == 69505:
-        Qjg3jQgVCqMeE6[6] = "$zk{OyEvj<Yx#h7%GhCDU^v92nIrwlyK=XwoNo4X^}0EHDrr-ior22a!4VZjF9@e&V(Uyp0b=MtBaD0vf&q1#U`!0$SmAp6<PU4D1"
-        LYBZtNRmqOYql0 = 56436
-    elif LYBZtNRmqOYql0 == 35338:
-        Qjg3jQgVCqMeE6[8] = "PN-p%p72e%&GeRPaWC<}%%s@NsEO0{Mu)TX{i{X}rrvl8wy)3X_m9h$_kM{V1;W5%q&F&*JKF&13cVy^2(CdeGE5ffodSpYvNWz;r"
-        LYBZtNRmqOYql0 = 88462
-    elif LYBZtNRmqOYql0 == 59602:
-        Qjg3jQgVCqMeE6[15] = "XkQi7Vr;%1EGd@f+LBEz<ZLD|tG)fh5AS~c$M<i(fBWt4e*EQkWFI{d=kJY-y%gmAoA2KJ_J^N-|F@t1`sQE%_{)#q|LWbJ|NhfA-"
-        LYBZtNRmqOYql0 = 75579
-    elif LYBZtNRmqOYql0 == 39978:
-        Qjg3jQgVCqMeE6[18] = "{GRSUt#O`xI9HK=b`A6HkL=c*$Aw}rJC%r>Wp=i$+VkI^o-hK8H+Hfmff;H@gh(=8;4P?wc$G_TiYRtVY+*{n-F_y<*bTSk2~Ukb?"
-        LYBZtNRmqOYql0 = 92992
-    elif LYBZtNRmqOYql0 == 88462:
-        Qjg3jQgVCqMeE6[9] = "9*QPY#K4dvjNCl55_d>yZlZr${krmM>6u!loC6XjETQN?^g-@e6N?7PbfyWYR#sV676mb`mfp5WD*Npwhqp>BNm?fNgu44uP4}(@r"
-        LYBZtNRmqOYql0 = 40109
-    elif LYBZtNRmqOYql0 == 40109:
-        Qjg3jQgVCqMeE6[10] = "Y}3SZcuWI_2d?8Ml*P8iqtG-hhb4g(*^%i+NoZNjHWQP(!nUo%@;%dyVWigU_YRK^9`8NeAAhm`<(AL|k)5~+PZ;+T*ky;l?Zog~S"
-        LYBZtNRmqOYql0 = 32451
-    elif LYBZtNRmqOYql0 == 36525:
-        Qjg3jQgVCqMeE6[30] = "6kN_kQX4EoCS|8qb@id=nUw;8DEXSG_lFA{ou%GY15iVL73gf>pw-nA?eghJYc0z4tLJ^YHDt?ME2p)KmwJ>#$H{eV?ZHzvvvW<hz"
-        LYBZtNRmqOYql0 = 41789
-    elif LYBZtNRmqOYql0 == 97640:
-        Qjg3jQgVCqMeE6[2] = "26pIw{H-IaK1vyeMl@BrVnOS=Ar*dJm^*MIPM0`z67gC|NBkQcneWnsPQTi{Vtffb^%ADv~r4M3)p=BS#~1R@M-8P!nE&`~HK6n5y"
-        LYBZtNRmqOYql0 = 72926
-    elif LYBZtNRmqOYql0 == 28039:
-        Qjg3jQgVCqMeE6[20] = "cC>eNZ#nXu4UT&UhsW)*9A0uA9^PDzu5cYaZWoUpujskC99`jf9JsVyzO{Gv@xd4E`f1lIDMHgm?Bucuo}Mhb&gltlhyjmCI%ona@"
-        LYBZtNRmqOYql0 = 26496
-    elif LYBZtNRmqOYql0 == 15643:
-        Qjg3jQgVCqMeE6[23] = "Z~Y-j^*_^e2CeQDN<zAB0rvKpj`lInD;aGu7i!ePAJmW}<AVkl?ye*cR4S8M98YuEAfIsRSOMsm0orjzHVjZx2~QBtC^>UCq)H3?g"
-        LYBZtNRmqOYql0 = 13323
-    elif LYBZtNRmqOYql0 == 25136:
-        Qjg3jQgVCqMeE6[25] = "+123-w&Y0b_uK9z^0>U1*Ln*ON6pQ$FHKRSW$xEO?-KA1V9OlEj~__<@=YMUigYgvpo6o!l1g1;$4Z-iiQ5M91edT<YqBE2;1%i>u"
-        LYBZtNRmqOYql0 = 52180
-    elif LYBZtNRmqOYql0 == 83264:
-        exec(jG0wAIJ6QIBxHV)
-        LYBZtNRmqOYql0 = 0
-    elif LYBZtNRmqOYql0 == 15251:
-        WlYdRPK4Lb2E6U = ''.join([Qjg3jQgVCqMeE6[i] for i in range(len(Qjg3jQgVCqMeE6))])
-        LYBZtNRmqOYql0 = 72572
-    elif LYBZtNRmqOYql0 == 85257:
-        Qjg3jQgVCqMeE6[5] = "q&Z>4w0tAFs0E3#B7Q!AFhZU@I0EfCyaz#&zQB)5GKiI?_m$R?os*;EJu)xTGi1WR0x9M)D>6zl7-W~szT#w7aB`mz-K3UdL}}gc^"
-        LYBZtNRmqOYql0 = 69505
-    elif LYBZtNRmqOYql0 == 63523:
-        Qjg3jQgVCqMeE6[13] = "{gLCnxIAn{te)iRM!>M52yETQ|TrM4NJPf^dRrP@~0W>nKcr*Hd~4u;RLNOqN2WM}Wz_QVK_`6vO(QNg~C<b)9{#_zzCm4kBQp^xN"
-        LYBZtNRmqOYql0 = 90058
-    elif LYBZtNRmqOYql0 == 61188:
-        Qjg3jQgVCqMeE6[12] = "3+7=f$AsF}FG5Bf>BQfr)@QK6QQH_AD0KejJBjbZD1IMk6oarf)MA*(OZ2sc8nnjHoTRMMD;HcmscsZI{_`h{W1<K!qPtKXwusy53"
-        LYBZtNRmqOYql0 = 63523
-    elif LYBZtNRmqOYql0 == 44694:
-        Qjg3jQgVCqMeE6[0] = "c-q}qTZ`Pt6@KquQIT1!-fi`i?oz{qjO{U=U0;S7+jtpf+a*=0Tee#2mDDpH8^bOMWJBKa5?ILNzJx$7FCk#^Yl7GN<iC)wO49UY+"
-        LYBZtNRmqOYql0 = 99127
-    elif LYBZtNRmqOYql0 == 92992:
-        Qjg3jQgVCqMeE6[19] = "1yM2z%B3r=+h)10idAr*sC$i@+<}$<b)|Csv<@Hxy!}IGkZd-A-;DW?ivEY)+0Nb)X+<kQK;67@SXb<=I?j5hhM!U2B<VoPkB{}k("
-        LYBZtNRmqOYql0 = 28039
-    elif LYBZtNRmqOYql0 == 10868:
-        Qjg3jQgVCqMeE6[17] = "^?LLGc;4@fks|jkr}jt}o^)h%%Ub8!?pfMO;fdT-dfzRf5$r7Us6~8dt0_$JrzWS@O`QfnicY=}7%;jY7^>*uu~-EyFg6Wslqte_P"
-        LYBZtNRmqOYql0 = 39978
-    elif LYBZtNRmqOYql0 == 37922:
-        Qjg3jQgVCqMeE6[27] = "d3MoULO6%@>v2N_UTMkR&a!Pnr&@kOfrgm->fMT92D~T^66;Cp$YNSS^?(ZC$icX&6=m*p!Au{(9S&RY9Nvn{q&NVq^S*^5qs-eV}"
-        LYBZtNRmqOYql0 = 49439
-    elif LYBZtNRmqOYql0 == 72926:
-        Qjg3jQgVCqMeE6[3] = "F`Vfh7tORP*q1y#3MT2GTjMoN*+=9!p@yk>O@%V3n0Sd{4m<k`|fQl*m|&XcT|r17Xn$fqP`d6B0v$n;Xgi?T}3M0O#0j0zqKK0w4"
-        LYBZtNRmqOYql0 = 93390
-    elif LYBZtNRmqOYql0 == 90058:
-        Qjg3jQgVCqMeE6[14] = "+iRn{2v~83HQYQpT()Rr>G5=3uR9f2Ja<@Gixk%DHRjpc|rDa)^)yi4BrJ=0$76>5GBF*@?J#F>=NEzE5zPsdAuj`K56unlIIJ91A"
-        LYBZtNRmqOYql0 = 59602
-    elif LYBZtNRmqOYql0 == 52180:
-        Qjg3jQgVCqMeE6[26] = "(C-%t0q_JTPHyau&ecDR(5wcfOC<+A9)<1G+KJZN{Wn_y4y*~pL8Cmg*Jw`u<w$2ajr|kfwXwgk+l-8br^aBF(b1x+)kwKX*!vbm!"
-        LYBZtNRmqOYql0 = 37922
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+IDA Pro 9.4 license keygen (cross-platform Python port of keygen.js).
+
+This ONLY generates the `idapro.hexlic` license file (the "keygen" part).
+It does NOT patch any binaries.
+
+Faithful reimplementation of the original keygen.js signing logic:
+  * canonical (recursively key-sorted, compact) JSON serialization
+  * SHA-256 over {"payload": ...}
+  * custom EMSA-style padding (hash placed at offset 95 in a 127-byte block,
+    XORed with a fixed 127-byte pad key)
+  * raw RSA private-key operation  sig = m^d mod N  (1024-bit)
+
+Integer conventions copied exactly from keygen.js:
+  * modulus N and private exponent d : little-endian byte order
+  * message block read as            : big-endian integer
+  * signature output bytes           : little-endian, padded to 128 bytes
+"""
+
+import hashlib
+import json
+
+# --- Embedded RSA key material (recovered private key) ----------------------
+C_MODULUS = bytes.fromhex(
+    "3f0307607fed562fd5a163adc40fcc603373caa28414e64cdc4552a555b13ad4"
+    "b3ad0a812800a03195300fd71634b90edb0d69ea710efebb2b0b9e72da2effb1"
+    "49de70bcbfa94b86af01ce455dbbd5fa987207651c7b60c2e4cafd0654188d98"
+    "c30f64dc084d8547f0ac32db91124af82b3b15bf922a31f1d5e332f27615cea7"
+)
+PRIVATE_KEY = bytes.fromhex(
+    "8b3f5fdfad7f87239734c530e2ecebeb4fa48d79518756c15fd54636801cf7ea"
+    "6367100566bf8b52b16bec05258d8426ea94c15841ab2d37802c07349df4c208"
+    "584e86d25a6bfb82966cb2ddcd3d654e9994e814ca470577362a937cc984e404"
+    "a0b68d173aab3180130118e1b03ed209a9d8757560a85a3c9b0d3380e7907c4f"
+)
+
+V54 = 127   # block length
+V56 = 95    # offset where the 32-byte hash is placed (95 + 32 == 127)
+PADKEY = bytes.fromhex(
+    "e2a7c300dfcc777f89b57500d8151c7fb1d97b3f9f170393311234ceeb9e377a"
+    "e2a7c300dfcc777f89b57500d8151c7fb1d97b3f9f170393311234ceeb9e377a"
+    "e2a7c300dfcc777f89b57500d8151c7fb1d97b3f9f170393311234ceeb9e377a"
+    "e2a7c300dfcc777f89b57500d8151c7fb1d97b3f9f170393311234ceeb9e37"
+)
+
+
+# --- License definition (mirrors keygen.js) ---------------------------------
+def build_license():
+    license = {
+        "header": {"version": 1},
+        "payload": {
+            "name": "来自：恒烈 EternalBlaze （若闲鱼买的请退款！）",
+            "email": "Henglie@vip.qq.com",
+            "licenses": [
+                {
+                    "description": "license",
+                    "edition_id": "ida-pro",
+                    "id": "14-0000-FFFF-88",
+                    "license_type": "named",
+                    "product": "IDA",
+                    "seats": 1,
+                    "start_date": "2024-08-10 00:00:00",
+                    "end_date": "2099-12-31 23:59:59",
+                    "issued_on": "2025-07-20 00:00:00",
+                    "owner": "来自：恒烈 EternalBlaze （若闲鱼买的请退款！）",
+                    "product_id": "IDAPRO",
+                    "product_version": "9.4",
+                    "add_ons": [],
+                    "features": [],
+                }
+            ],
+        },
+    }
+    add_addons(license)
+    return license
+
+
+def add_addons(license):
+    addons = [  # update as needed, doesn't include cloud add-ons
+        "LUMINA", "TEAMS",
+        "HEXX86", "HEXX64", "HEXARM", "HEXARM64",
+        "HEXMIPS", "HEXMIPS64", "HEXPPC", "HEXPPC64",
+        "HEXRV", "HEXRV64", "HEXARC", "HEXARC64",
+        "HEXV850", "HEXDALVIK",
+    ]
+    lic0 = license["payload"]["licenses"][0]
+    for i, addon in enumerate(addons):
+        lic0["add_ons"].append({
+            "id": "48-1337-B00B-%02d" % (i + 1),
+            "code": addon,
+            "owner": lic0["id"],
+            "start_date": "2025-07-20 00:00:00",
+            "end_date": "2099-12-31 23:59:59",
+        })
+
+
+# --- Helper functions -------------------------------------------------------
+def canonical(obj):
+    """Recursive key-sorted, compact JSON string (matches JS `sort()`)."""
+    if isinstance(obj, list):
+        return "[" + ",".join(canonical(x) for x in obj) + "]"
+    if isinstance(obj, dict):
+        keys = sorted(obj.keys())
+        return "{" + ",".join('"' + k + '":' + canonical(obj[k]) for k in keys) + "}"
+    # primitive: bool/int/float/str/None -> same textual form as JSON.stringify
+    return json.dumps(obj, ensure_ascii=False, separators=(",", ":"))
+
+
+def encrypt(message: bytes) -> bytes:
+    """Raw RSA private operation, replicating keygen.js integer conventions."""
+    modulus = int.from_bytes(C_MODULUS, "little")
+    key = int.from_bytes(PRIVATE_KEY, "little")
+    msg = int.from_bytes(message, "big")  # == little-endian of reversed(message)
+
+    base = msg % modulus
+    encrypted = pow(base, key, modulus)
+
+    # output little-endian, trimmed of high zero bytes (JS pushes LSB first)
+    length = (encrypted.bit_length() + 7) // 8
+    return encrypted.to_bytes(length, "little")
+
+
+def sign(payload) -> str:
+    data_str = canonical({"payload": payload})
+    digest = hashlib.sha256(data_str.encode("utf-8")).digest()  # 32 bytes
+
+    U = bytearray(V54)
+    U[V56:V56 + len(digest)] = digest  # place hash at offset 95
+    block = bytearray(V54)
+    for i in range(V54):
+        block[i] = U[i] ^ PADKEY[i]
+    if block[0] == 0:
+        block[0] ^= 1
+
+    sig = encrypt(bytes(block))
+    out = bytearray(128)
+    out[0:len(sig)] = sig  # left-aligned, zero padded to 128 bytes
+    return out.hex().upper()
+
+
+def main():
+    license = build_license()
+    license["signature"] = sign(license["payload"])
+
+    text = canonical(license)
+    with open("idapro.hexlic", "w", encoding="utf-8") as f:
+        f.write(text)
+    print("License written to idapro.hexlic")
+    print("Signature:", license["signature"])
+
+
+if __name__ == "__main__":
+    main()
